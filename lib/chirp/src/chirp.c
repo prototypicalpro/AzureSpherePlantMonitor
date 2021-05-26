eeaa7755f39a4617697ee201a6d0a49a9ae249f3
Noah Laptop <noah@koontzs.com>
2021-05-25 18:16:18 -0700

    fix: switch chirps to more accurate sampling technique

--- lib/chirp/src/chirp.c
+++ lib/chirp/src/chirp.c
@@ -1,15 +1,12 @@
+#include <time.h>
+#include <string.h>
+
 #include "chirp.h"
 
 typedef union {
 	uint16_t u16bit;
 	uint8_t u8bit[2];
 } axis1bit16_t;
-
-inline static void swap(uint8_t* a, uint8_t* b) {
-	uint8_t c = *a;
-	*a = *b;
-	*b = c;
-}
 
 int ChirpInit(chirp_t* chirp, int i2c_fd, I2C_DeviceAddress addr) {
 	chirp->_fd = i2c_fd;
@@ -19,9 +16,8 @@ int ChirpInit(chirp_t* chirp, int i2c_fd, I2C_DeviceAddress addr) {
 	axis1bit16_t out;
 	const uint8_t reg = 0;
 	int ret = I2CMaster_WriteThenRead(chirp->_fd, addr, &reg, 1, out.u8bit, 2);
-	swap(out.u8bit, out.u8bit + 1);
-	if (ret < 0 || out.u16bit > 10000U) {
-		Log_Debug("Soil sensor with addr %i not found!\n", chirp->_addr);
+	if (ret < 0 || out.u16bit != 1) {
+		Log_Debug("Soil sensor with addr %i not found! Error: %s\n", chirp->_addr, strerror(errno));
 		return -1;
 	}
 
@@ -33,13 +29,29 @@ int ChirpMeasure(chirp_t* chirp, chirp_data_t* data_out) {
 	// read data!
 	if (!chirp->_is_active)
 		return -1;
-	//  first sensor
+
 	axis1bit16_t out;
-	const uint8_t reg = 0;
+	uint8_t reg = 0;
+	// trigger sensor
 	int ret = I2CMaster_WriteThenRead(chirp->_fd, chirp->_addr, &reg, 1, out.u8bit, 2);
-	swap(out.u8bit, out.u8bit + 1);
-	if (ret < 0 || out.u16bit > 10000U) {
+	if (ret < 0 || out.u16bit != 1) {
 		Log_Debug("Soil sensor with addr %i not found!\n", chirp->_addr);
+		chirp->_is_active = false;
+		return -1;
+	}
+
+	// wait for cycle to complete
+	const struct timespec sleep_time = { .tv_sec = 2, .tv_nsec = 0 };
+	struct timespec sleep_time_rem = {};
+	int err = clock_nanosleep(CLOCK_MONOTONIC, 0, &sleep_time, &sleep_time_rem);
+	if (err == EINTR)
+		clock_nanosleep(CLOCK_MONOTONIC, 0, &sleep_time_rem, NULL);
+
+	// read value
+	reg = 1;
+	ret = I2CMaster_WriteThenRead(chirp->_fd, chirp->_addr, &reg, 1, out.u8bit, 2);
+	if (ret < 0 || out.u16bit > 10000U) {
+		Log_Debug("Soil sensor with addr %i did not read correctly!\n", chirp->_addr);
 		chirp->_is_active = false;
 		return -1;
 	}
